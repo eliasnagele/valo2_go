73f9c2a5fe9c6c0a5e1884bfe295b8d8986265f7
Eugene JansenVanRensburg <eu.jansenvanrensburg@hak-kitz.ac.at>
2022-12-02 09:46:31 +0100

    Eugenepush

--- SWP Jahresprojekt/Form1.cs
+++ SWP Jahresprojekt/Form1.cs
@@ -6,5 +6,38 @@ namespace SWP_Jahresprojekt
         {
             InitializeComponent();
         }
+
+        private void bt_tournament_Click(object sender, EventArgs e)
+        {
+            tournament tournament = new tournament();
+            this.Hide();
+            tournament.Show();
+        }
+
+        private void bt_news_Click(object sender, EventArgs e)
+        {
+            news news = new news();
+            this.Hide();
+            news.Show();
+        }
+
+        private void bt_skins_Click(object sender, EventArgs e)
+        {
+            skins skins = new skins();
+            this.Hide();
+            skins.Show();
+        }
+
+        private void bt_bundles_Click(object sender, EventArgs e)
+        {
+            Bundles Bundles = new Bundles();
+            this.Hide();
+            Bundles.Show();
+        }
+
+        private void bt_exit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
     }
 }
---
+++ SWP Jahresprojekt/skins.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SWP_Jahresprojekt
+{
+    public partial class skins : Form
+    {
+        public skins()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_home_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            this.Hide();
+            form1.Show();
+        }
+
+        private void bt_exit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
