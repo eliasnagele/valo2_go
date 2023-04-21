d2b93ff45e084d1838d940029b5c6ce9e1c48379
Elias Nagele <elias.nagele@hak-kitz.ac.at>
2023-04-21 10:01:48 +0200

    21.04. BundleDetails weitergearbeitet

--- SWP Jahresprojekt/BundleDetails.Designer.cs
+++ SWP Jahresprojekt/BundleDetails.Designer.cs
@@ -28,46 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
+            pb_BundleDetails = new PictureBox();
+            lb_BundleDetailsName = new Label();
+            lb_BundleDetailsPrice = new Label();
+            lb_BundleDetailsRarity = new Label();
+            lb_BundleDetailsDate = new Label();
+            lb_BundleDetailsVariants = new Label();
+            tB_BundleDetailsDate = new TextBox();
+            tB_BundleDetailsVariants = new TextBox();
+            tB_BundleDetailsName = new TextBox();
+            tB_BundleDetailsPrice = new TextBox();
+            tB_BundleDetailsRarity = new TextBox();
+            lb_headline = new Label();
+            bt_exit = new Button();
+            bt_back = new Button();
+            ((System.ComponentModel.ISupportInitialize)pb_BundleDetails).BeginInit();
+            SuspendLayout();
             // 
-            // pictureBox1
+            // pb_BundleDetails
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(225, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(347, 192);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            pb_BundleDetails.Location = new Point(12, 72);
+            pb_BundleDetails.Name = "pb_BundleDetails";
+            pb_BundleDetails.Size = new Size(776, 152);
+            pb_BundleDetails.TabIndex = 0;
+            pb_BundleDetails.TabStop = false;
             // 
-            // label1
+            // lb_BundleDetailsName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 226);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            lb_BundleDetailsName.AutoSize = true;
+            lb_BundleDetailsName.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lb_BundleDetailsName.Location = new Point(128, 271);
+            lb_BundleDetailsName.Name = "lb_BundleDetailsName";
+            lb_BundleDetailsName.Size = new Size(52, 21);
+            lb_BundleDetailsName.TabIndex = 1;
+            lb_BundleDetailsName.Text = "Name";
+            // 
+            // lb_BundleDetailsPrice
+            // 
+            lb_BundleDetailsPrice.AutoSize = true;
+            lb_BundleDetailsPrice.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lb_BundleDetailsPrice.Location = new Point(128, 328);
+            lb_BundleDetailsPrice.Name = "lb_BundleDetailsPrice";
+            lb_BundleDetailsPrice.Size = new Size(44, 21);
+            lb_BundleDetailsPrice.TabIndex = 2;
+            lb_BundleDetailsPrice.Text = "Price";
+            // 
+            // lb_BundleDetailsRarity
+            // 
+            lb_BundleDetailsRarity.AutoSize = true;
+            lb_BundleDetailsRarity.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lb_BundleDetailsRarity.Location = new Point(128, 390);
+            lb_BundleDetailsRarity.Name = "lb_BundleDetailsRarity";
+            lb_BundleDetailsRarity.Size = new Size(51, 21);
+            lb_BundleDetailsRarity.TabIndex = 3;
+            lb_BundleDetailsRarity.Text = "Rarity";
+            // 
+            // lb_BundleDetailsDate
+            // 
+            lb_BundleDetailsDate.AutoSize = true;
+            lb_BundleDetailsDate.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lb_BundleDetailsDate.Location = new Point(418, 271);
+            lb_BundleDetailsDate.Name = "lb_BundleDetailsDate";
+            lb_BundleDetailsDate.Size = new Size(42, 21);
+            lb_BundleDetailsDate.TabIndex = 4;
+            lb_BundleDetailsDate.Text = "Date";
+            // 
+            // lb_BundleDetailsVariants
+            // 
+            lb_BundleDetailsVariants.AutoSize = true;
+            lb_BundleDetailsVariants.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lb_BundleDetailsVariants.Location = new Point(418, 328);
+            lb_BundleDetailsVariants.Name = "lb_BundleDetailsVariants";
+            lb_BundleDetailsVariants.Size = new Size(66, 21);
+            lb_BundleDetailsVariants.TabIndex = 5;
+            lb_BundleDetailsVariants.Text = "Variants";
+            // 
+            // tB_BundleDetailsDate
+            // 
+            tB_BundleDetailsDate.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            tB_BundleDetailsDate.Location = new Point(535, 271);
+            tB_BundleDetailsDate.Name = "tB_BundleDetailsDate";
+            tB_BundleDetailsDate.ReadOnly = true;
+            tB_BundleDetailsDate.Size = new Size(150, 25);
+            tB_BundleDetailsDate.TabIndex = 11;
+            // 
+            // tB_BundleDetailsVariants
+            // 
+            tB_BundleDetailsVariants.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            tB_BundleDetailsVariants.Location = new Point(535, 328);
+            tB_BundleDetailsVariants.Multiline = true;
+            tB_BundleDetailsVariants.Name = "tB_BundleDetailsVariants";
+            tB_BundleDetailsVariants.ReadOnly = true;
+            tB_BundleDetailsVariants.Size = new Size(150, 87);
+            tB_BundleDetailsVariants.TabIndex = 12;
+            // 
+            // tB_BundleDetailsName
+            // 
+            tB_BundleDetailsName.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            tB_BundleDetailsName.Location = new Point(210, 271);
+            tB_BundleDetailsName.Name = "tB_BundleDetailsName";
+            tB_BundleDetailsName.ReadOnly = true;
+            tB_BundleDetailsName.Size = new Size(150, 25);
+            tB_BundleDetailsName.TabIndex = 13;
+            // 
+            // tB_BundleDetailsPrice
+            // 
+            tB_BundleDetailsPrice.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            tB_BundleDetailsPrice.Location = new Point(210, 328);
+            tB_BundleDetailsPrice.Name = "tB_BundleDetailsPrice";
+            tB_BundleDetailsPrice.ReadOnly = true;
+            tB_BundleDetailsPrice.Size = new Size(150, 25);
+            tB_BundleDetailsPrice.TabIndex = 14;
+            // 
+            // tB_BundleDetailsRarity
+            // 
+            tB_BundleDetailsRarity.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            tB_BundleDetailsRarity.Location = new Point(210, 390);
+            tB_BundleDetailsRarity.Name = "tB_BundleDetailsRarity";
+            tB_BundleDetailsRarity.ReadOnly = true;
+            tB_BundleDetailsRarity.Size = new Size(150, 25);
+            tB_BundleDetailsRarity.TabIndex = 15;
+            // 
+            // lb_headline
+            // 
+            lb_headline.AutoSize = true;
+            lb_headline.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            lb_headline.Location = new Point(313, 19);
+            lb_headline.Name = "lb_headline";
+            lb_headline.Size = new Size(168, 32);
+            lb_headline.TabIndex = 16;
+            lb_headline.Text = "Bundle Details";
+            // 
+            // bt_exit
+            // 
+            bt_exit.Location = new Point(12, 8);
+            bt_exit.Name = "bt_exit";
+            bt_exit.Size = new Size(75, 23);
+            bt_exit.TabIndex = 17;
+            bt_exit.Text = "Exit";
+            bt_exit.UseVisualStyleBackColor = true;
+            bt_exit.Click += bt_exit_Click;
+            // 
+            // bt_back
+            // 
+            bt_back.Location = new Point(12, 37);
+            bt_back.Name = "bt_back";
+            bt_back.Size = new Size(75, 23);
+            bt_back.TabIndex = 18;
+            bt_back.Text = "Back";
+            bt_back.UseVisualStyleBackColor = true;
+            bt_back.Click += bt_back_Click;
             // 
             // BundleDetails
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
-            this.Name = "BundleDetails";
-            this.Text = "BundleDetails";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(bt_back);
+            Controls.Add(bt_exit);
+            Controls.Add(lb_headline);
+            Controls.Add(tB_BundleDetailsRarity);
+            Controls.Add(tB_BundleDetailsPrice);
+            Controls.Add(tB_BundleDetailsName);
+            Controls.Add(tB_BundleDetailsVariants);
+            Controls.Add(tB_BundleDetailsDate);
+            Controls.Add(lb_BundleDetailsVariants);
+            Controls.Add(lb_BundleDetailsDate);
+            Controls.Add(lb_BundleDetailsRarity);
+            Controls.Add(lb_BundleDetailsPrice);
+            Controls.Add(lb_BundleDetailsName);
+            Controls.Add(pb_BundleDetails);
+            Name = "BundleDetails";
+            Text = "BundleDetails";
+            ((System.ComponentModel.ISupportInitialize)pb_BundleDetails).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private PictureBox pictureBox1;
-        private Label label1;
+        private PictureBox pb_BundleDetails;
+        private Label lb_BundleDetailsName;
+        private Label lb_BundleDetailsPrice;
+        private Label lb_BundleDetailsRarity;
+        private Label lb_BundleDetailsDate;
+        private Label lb_BundleDetailsVariants;
+        private TextBox tB_BundleDetailsDate;
+        private TextBox tB_BundleDetailsVariants;
+        private TextBox tB_BundleDetailsName;
+        private TextBox tB_BundleDetailsPrice;
+        private TextBox tB_BundleDetailsRarity;
+        private Label lb_headline;
+        private Button bt_exit;
+        private Button bt_back;
     }
 }
--- SWP Jahresprojekt/BundleDetails.cs
+++ SWP Jahresprojekt/BundleDetails.cs
@@ -16,14 +16,26 @@ namespace SWP_Jahresprojekt
         {
             InitializeComponent();
 
+            List<string> bundledetails = new List<string>();
+
             if (Bundles.BundleID.Equals(1))
             {
-
+                bundledetails = SQLInformation.BundleDetails();
             }
             else if (Bundles.BundleID.Equals(2))
             {
 
             }
+        }
+
+        private void bt_back_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void bt_exit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
